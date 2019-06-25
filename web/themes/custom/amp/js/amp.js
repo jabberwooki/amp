b5dacfe5c2897b26c9ace76fc523691591c7c38b
Christophe Espiau <christophe.espiau@gmail.com>
2019-06-25 14:11:35 +0200

    Abandon du nouveau formateur 'Image adaptative avec une légende à partir du titre' (à débugger plus tard). Remplacé par du code jquery pour afficher une légende d'image responsive à partir de l'attribut title."

--- web/themes/custom/amp/js/amp.js
+++ web/themes/custom/amp/js/amp.js
@@ -1,11 +1,14 @@
 (function ($) {
     Drupal.behaviors.news = {
       attach: function (context, settings) {
+        //Actus hauteur titre
+        $(".wrapper-title").matchHeight();
+        $(".node--type-news.node--view-mode-teaser .field--name-field-summary").matchHeight();
 
-    //Actus hauteur titre
-    $(".wrapper-title").matchHeight();
-    $(".node--type-news.node--view-mode-teaser .field--name-field-summary").matchHeight();
-
-        }
+        console.log('dans amp.js');
+        var legend = $(".region-highlighted picture img").attr('title');
+        console.log(legend);
+        $(".region-highlighted picture").after('<div class="image-caption">' + legend  + '</div>');
+      }
     };
 })(jQuery);
